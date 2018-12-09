1d98c73a4a99752dc806442629d25472caa3d190
Jack <18008258751@163.com>
2018-12-09 18:20:54 +0800

    添加一个单元测试

--- src/test/java/com/bxia/spring/boot/docker/SpringBootDockerApplicationTests.java
+++ src/test/java/com/bxia/spring/boot/docker/SpringBootDockerApplicationTests.java
@@ -5,13 +5,24 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBootDockerApplicationTests {
+
+    @Resource
+    private SpringBootDockerApplication application;
 
     @Test
     public void contextLoads() {
         System.out.println("Hello World.");
+    }
+
+    @Test
+    public void test01(){
+        String owner = application.owner();
+        System.out.println(owner);
     }
 
 }
